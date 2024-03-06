64bb4d995bca5647ce283585db8e25949e7133c4
khankluan24 <longtran24062000@gmail.com>
2024-03-06 18:18:20 +0700

    fix: Fix mismatch public key

--- src/pages/(public)/_components/hero/index.tsx
+++ src/pages/(public)/_components/hero/index.tsx
@@ -1,15 +1,13 @@
-import React, { useCallback, useMemo } from "react"
+import React, { useCallback } from "react"
 import Image from "next/image"
 import { keypairIdentity, Metaplex } from "@metaplex-foundation/js"
 import {
-  create,
   fetchCandyGuard,
   fetchCandyMachine,
   getMerkleRoot,
   mintV2,
   mplCandyMachine,
   safeFetchCandyGuard,
-  updateCandyGuard,
 } from "@metaplex-foundation/mpl-candy-machine"
 import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox"
 import {
@@ -21,59 +19,56 @@ import {
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters"
 import { useWallet } from "@solana/wallet-adapter-react"
-import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js"
+import { clusterApiUrl, Connection, Keypair } from "@solana/web3.js"
 
-// import base58 from "bs58"
-
-const keypair = Keypair.generate()
-
-const METAPLEX = Metaplex.make(new Connection(clusterApiUrl("devnet"))).use(
-  keypairIdentity(keypair)
-)
+const CANDY_MACHINE_ID = "FQatJTLee4XsRg5AM2QCkj7WmmjDwdcX6SvJQayZF9Y5"
+const allowList = [
+  "4s6rNxhiLQ1VJbDSi1JsNE9Eoo845r1rT4MWuUnZetHA",
+  "ATEPjBj4j36wA1XB12X1crvbXMpj3D3q9HgKgXuY9MFM",
+]
 const treasury = publicKey("HEKx4kKe3GMmEnFk7joddscwW4EGRq6BM8ne2KwX7GWr")
 
 const Hero = () => {
   const wallet = useWallet()
+  const umi = createUmi(clusterApiUrl("devnet"))
+    .use(walletAdapterIdentity(wallet))
+    .use(mplCandyMachine())
 
   const onClick = useCallback(async () => {
-    const allowList = [
-      "ATEPjBj4j36wA1XB12X1crvbXMpj3D3q9HgKgXuY9MFM",
-      "4s6rNxhiLQ1VJbDSi1JsNE9Eoo845r1rT4MWuUnZetHA",
-    ]
-    const CANDY_MACHINE_ID = "FQatJTLee4XsRg5AM2QCkj7WmmjDwdcX6SvJQayZF9Y5"
     const candyMachineAddress = publicKey(CANDY_MACHINE_ID)
-    const umi = createUmi(clusterApiUrl("devnet"))
-      .use(walletAdapterIdentity(wallet))
-      .use(mplCandyMachine())
     const candyMachine = await fetchCandyMachine(umi, candyMachineAddress)
-    const candyGuard = await fetchCandyGuard(umi, candyMachine.mintAuthority)
+    const candyGuard = await safeFetchCandyGuard(
+      umi,
+      candyMachine.mintAuthority
+    )
 
     try {
-      // Mint from the Candy Machine.
       const nftMint = generateSigner(umi)
       const transaction = await transactionBuilder()
         .add(setComputeUnitLimit(umi, { units: 800_000 }))
         .add(
           mintV2(umi, {
             candyMachine: candyMachine.publicKey,
-            nftMint: nftMint,
-            candyGuard: candyGuard.publicKey,
+            candyGuard: candyGuard?.publicKey,
+            nftMint,
             collectionMint: candyMachine.collectionMint,
             collectionUpdateAuthority: candyMachine.authority,
             group: some("WL"),
             mintArgs: {
-              // solPayment: some({ destination: treasury }),
+              solPayment: some({ destination: treasury }),
               allowList: some({
                 merkleRoot: getMerkleRoot(allowList),
               }),
             },
           })
         )
-        .sendAndConfirm(umi)
+      const { signature } = await transaction.sendAndConfirm(umi, {
+        confirm: { commitment: "confirmed" },
+      })
     } catch (error: any) {
       console.error("error", `Mint failed! ${error?.message}`)
     }
-  }, [wallet])
+  }, [umi])
   return (
     <div className="flex flex-col items-center mt-10 mb-12 gap-5">
       <Image
@@ -98,6 +93,12 @@ const Hero = () => {
 }
 
 export default Hero
+
+// const keypair = Keypair.generate()
+
+// const METAPLEX = Metaplex.make(new Connection(clusterApiUrl("devnet"))).use(
+//   keypairIdentity(keypair)
+// )
 
 const mintNft = async () => {
   // const candyMachine = await METAPLEX.candyMachines().findByAddress({
